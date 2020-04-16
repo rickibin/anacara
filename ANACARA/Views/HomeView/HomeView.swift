7f4cc02321c31c6547a114feb02c15c4d968922e
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-04-16 21:39:13 +0700

    adding structure for home

---
+++ ANACARA/Views/HomeView/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeView.swift
+//  ANACARA
+//
+//  Created by Ricki Bin Yamin on 16/04/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: View {
+    override func setViews() {
+        super.setViews()
+        
+    }
+}
