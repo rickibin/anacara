169abaf7153688710992142f1cbf819346b54edb
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-05-12 23:49:41 +0700

    learnd tobe continued

--- ANACARA/Views/LearnView/Learn4View.swift
+++ ANACARA/Views/LearnView/Learn4View.swift
@@ -9,6 +9,46 @@
 import UIKit
 
 class Learn4View: View {
+    
+    var delegate: Learn4ViewDelegate?
+    
+    lazy var customNavBarView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        
+        return view
+    }()
+    
+    lazy var closeButton: AnimatingButton = {
+        let button = AnimatingButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "CloseButton"), for: .normal)
+        //button.addTarget(self, action: #selector(closeButtonTapped(_:)), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var progressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 67/255, green: 67/255, blue: 67/255, alpha: 1)
+        label.font = .systemFont(ofSize: 23, weight: .medium)
+        label.text = "1/10"
+        
+        return label
+    }()
+    
+    lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 67/255, green: 67/255, blue: 67/255, alpha: 1)
+        label.font = .systemFont(ofSize: 23, weight: .bold)
+        label.text = "Pilihen aksara kanggo \"ka\""
+        
+        return label
+    }()
+    
     override func setViews() {
         super.setViews()
         
---
+++ ANACARA/Views/LearnView/Learn4ViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  Learn4ViewDelegate.swift
+//  ANACARA
+//
+//  Created by Ricki Bin Yamin on 12/05/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+protocol Learn4ViewDelegate {
+    
+}
