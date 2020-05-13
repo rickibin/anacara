5cb072886395f93d5ef17d5787acf760b928578c
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-05-13 19:11:27 +0700

    Learn level view done

---
+++ ANACARA/Controllers/LearnViewController/LearnLevelViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LearnLevelViewController.swift
+//  ANACARA
+//
+//  Created by Ricki Bin Yamin on 13/05/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class LearnLevelViewController: ViewController<LearnLevelView> {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
