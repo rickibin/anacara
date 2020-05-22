d767a15029d6f2228e55a8a727060787f0fa80a8
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-05-22 13:31:30 +0700

    AVAudioFoundation added

--- ANACARA/Controllers/HomeViewController/HomeViewController.swift
+++ ANACARA/Controllers/HomeViewController/HomeViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: ViewController<HomeView> {
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         screenView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        playSound(name: "background")
+    }
+    
+    func playSound(name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
 
 extension HomeViewController: HomeViewDelegate {
     func sinauButtonTapped(_ view: View, didTapButton button: AnimatingButton) {
+        playSound(name: "button")
         let vc = UINavigationController(rootViewController: LearnLevelViewController())
         vc.modalPresentationStyle = .fullScreen
         
@@ -24,6 +56,7 @@ extension HomeViewController: HomeViewDelegate {
     }
     
     func nulisButtonTapped(_ view: View, didTapButton button: AnimatingButton) {
+        playSound(name: "button")
         let vc = UINavigationController(rootViewController: WriteLevelViewController())
         vc.modalPresentationStyle = .fullScreen
         
--- ANACARA/Controllers/WriteViewController/WriteLevelViewController.swift
+++ ANACARA/Controllers/WriteViewController/WriteLevelViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class WriteLevelViewController: ViewController<WriteLevelView> {
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         screenView.delegate = self
@@ -54,6 +58,28 @@ class WriteLevelViewController: ViewController<WriteLevelView> {
                 screenView.level4Button.isEnabled = false
                 break
             }
+        }
+    }
+    
+    func playSound(name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
 }
